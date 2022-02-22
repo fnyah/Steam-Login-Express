c1ef1a76a6fd1ffe562e5d260452021b3f61620c
fnyah <zackbennett@Live.ca>
2022-02-22 13:13:46 -0500

    Cleand up code and removed many unused components

--- app.js
+++ app.js
@@ -1,56 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport');
-var util = require('util');
-var session = require('express-session');
-var SteamStrategy = require("passport-steam").Strategy;
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const util = require('util');
+const session = require('express-session');
+const SteamStrategy = require("passport-steam").Strategy;
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new SteamStrategy({
-  returnURL: 'http://localhost:3000/auth/steam/return',
-  realm: 'http://localhost:3000/',
-  apiKey: '' // put steam api key here
-},
-function(identifier, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-
-    // To keep the example simple, the user's Steam profile is returned to
-    // represent the logged-in user.  In a typical application, you would want
-    // to associate the Steam account with a user record in your database,
-    // and return that user instead.
-    profile.identifier = identifier;
-    console.log(profile)
-    return done(null, profile);
-
-  });
-}
-));
-
-
-var app = express();
-
+const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -61,8 +30,6 @@ app.use(session({
   saveUninitialized: true
 }));
 
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + './public'));
@@ -70,36 +37,64 @@ app.use(express.static(__dirname + './public'));
 app.use('/', indexRouter);
 app.use('/account', indexRouter);
 
-
 app.get('/auth/steam',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
+  passport.authenticate('steam', {
+    failureRedirect: '/'
+  }),
+  function (req, res) {
     res.redirect('/');
   });
 
 app.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
+  passport.authenticate('steam', {
+    failureRedirect: '/'
+  }),
+  function (req, res) {
     res.redirect('/');
   });
 
-// app.listen(3000);
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:3000/auth/steam/return',
+    realm: 'http://localhost:3000/',
+    apiKey: '' // Your steam api key goes here!
+  },
+  function (identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      /* To keep the example simple, the user's Steam profile is returned to
+      represent the logged-in user.  In a typical application, you would want
+      to associate the Steam account with a user record in your database,
+      and return that user instead. */
+      profile.identifier = identifier;
+      // console.log(profile)
+      return done(null, profile);
+    });
+  }
+));
+
+app.listen(3000)
 console.log("listening");
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var app = express();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { user: req.user });
@@ -17,11 +15,9 @@ router.get('/logout', function(req, res, next) {
   res.redirect('/');
 });
 
-
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/');
 }
-
 
 module.exports = router;
